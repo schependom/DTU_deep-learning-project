d9432bddd591e3020a8cf5eeb6e23d97c67e7f69
Vincent Van Schependom <vincent.van.schependom@gmail.com>
2025-11-03 12:43:08 +0100

    Train local script

--- src/train-local-maze.sh
+++ src/train-local-maze.sh
@@ -1,5 +1,5 @@
 run_name="pretrain_att_maze30x30"
-torchrun --nproc-per-node 4 --rdzv_backend=c10d --rdzv_endpoint=localhost:0 --nnodes=1 pretrain.py \
+python pretrain.py \
 arch=trm \
 data_paths="[data/maze-30x30-hard-1k]" \
 evaluators="[]" \
