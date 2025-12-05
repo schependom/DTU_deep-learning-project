f766419a467025ef6e110c2fc83dc60de94b1dd9
maltheaugust <s214631@dtu.dk>
2025-12-05 20:20:35 +0100

    updated chess job script

--- job-chess.sh
+++ job-chess.sh
@@ -25,15 +25,9 @@ source ${REPO}/.venv/bin/activate
 export MASTER_ADDR=$(hostname)
 export MASTER_PORT=29500 
 export DATE_TAG=$(date +%Y%m%d_%H%M)
-
-
-# -------------------------------------------
-# Chess Dataset Parameters
-# -------------------------------------------
-
-export CSV_PATH="dataset/chess_moves.csv"   # <--- path to your CSV
+export CSV_PATH="src/sdataset/chess_moves.csv"  
 export OUT_NAME="chess"
-export OUT_DIR="data/${OUT_NAME}"
+export OUT_DIR="src/data/${OUT_NAME}"
 
 export RUN_NAME="${OUT_NAME}_${DATE_TAG}"
 
@@ -43,10 +37,7 @@ echo " Run Name: ${RUN_NAME}"
 echo "---------------------------------------------------------"
 
 
-# -------------------------------------------
-# Dataset discovery / generation
-# -------------------------------------------
-
+#Check for the data-set
 if [ -d "${OUT_DIR}" ]; then
     export DATA_PATH="${OUT_DIR}"
     echo "Found existing dataset at ${DATA_PATH}"
@@ -61,49 +52,31 @@ else
 fi
 
 
-# -------------------------------------------
-# Create Run Directory
-# -------------------------------------------
-
 mkdir -p "${REPO}/checkpoints/${RUN_NAME}"
 
-
-# -------------------------------------------
-# TRM Architecture Notes for Chess
-# -------------------------------------------
-# - seq_len = 1 + 80 ≈ 81 tokens (similar to Sudoku)
-# - vocab_size ~ FEN(≈40) + moves(4096)
-# - Reasoning model should use transformer (mlp_t=False)
-# - pos_encodings = rope or learned (Rope works well)
-# - Evaluator: For now use "none" or a simple accuracy evaluator
-# -------------------------------------------
-
-
-# -------------------------------------------
-# Start Training
-# -------------------------------------------
+# Training
 
 echo "Starting Training Job: ${RUN_NAME}"
-torchrun --nproc-per-node 1 --rdzv_backend=c10d --nnodes=1 ${REPO}/src/pretrain.py \
+torchrun --standalone --nproc-per-node 1 ${REPO}/src/pretrain.py \
     arch=trm \
     data_paths="[${DATA_PATH}]" \
     arch.L_layers=2 \
     arch.H_cycles=3 \
     arch.L_cycles=6 \
     evaluators="[{name: chess@ChessEvaluator}]" \
-    epochs=500 \
+    epochs=1000 \
     eval_interval=1 \
     min_eval_interval=0 \
-    global_batch_size=128 \
+    global_batch_size=512 \
     lr=1e-4 \
-    puzzle_emb_lr=1e-4 \
+    puzzle_emb_lr=5e-4 \
     lr_warmup_steps=1000 \
     weight_decay=1.0 \
     puzzle_emb_weight_decay=1.0 \
     +run_name=${RUN_NAME} \
     ema=True \
     arch.mlp_t=False \
-    arch.pos_encodings=None
+    arch.pos_encodings=rope
 
 echo "Job finished at:"
 date
